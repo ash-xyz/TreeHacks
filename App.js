7ecc58a8f1bcb031809f4b77f4233458032171fa
1auren-kim <your_email@youremail.com>
2023-02-18 15:27:31 -0800

    Changed text to Project

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi Treehacks!</Text>
+      <Text>Treehacks</Text>
       <StatusBar style="auto" />
     </View>
   );
