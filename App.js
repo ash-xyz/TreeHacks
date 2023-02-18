c6f5e5c049ea2cee7ce404c0fc6c1dd6203a11f0
1auren-kim <your_email@youremail.com>
2023-02-18 15:38:05 -0800

    bruh

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Fetch?</Text>
+      <Text>bruh</Text>
       <StatusBar style="auto" />
     </View>
   );
