09e54536606d8c9a140c6794989e2596fab8d165
Ashraf Ali <ashrafali@Ashrafs-MacBook-Pro.local>
2023-02-19 09:39:10 -0800

    fix

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import CourseList from './Components/CourseList';
 import React from 'react';
 
