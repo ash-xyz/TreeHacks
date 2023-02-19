c4b427e01ec456b07ec6b32dabd966c32e658f3d
megankulshekar <91085343+megankulshekar@users.noreply.github.com>
2023-02-19 10:29:45 -0700

    Adding rounded borders

--- App.js
+++ App.js
@@ -5,9 +5,13 @@ import React from 'react';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, styles.playingSpace]}>
+      <TouchableOpacity style={{ backgroundColor: '#147EFB', padding: 10, borderRadius: 4 }}>
+        <Text style={{ color: '#fff' }}>Click me!</Text>
+      </TouchableOpacity>
       <CourseList />
     </View>
+    
   );
 }
 
@@ -25,5 +29,10 @@ const styles = StyleSheet.create({
     textShadowColor: 'red',
     textShadowRadius: 1,
     margin: 24,
-  }
+  },
+  playingSpace: {
+    backgroundColor: 'white',
+    borderColor: 'black',
+    borderWidth: 3,
+  },
 });
