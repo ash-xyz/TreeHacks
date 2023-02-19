691a5ad3b909093a9e2a18e30f5ecd857326a8c4
Ashraf Ali <ashrafali@Ashrafs-MacBook-Pro.local>
2023-02-19 08:32:49 -0800

    Added scrollable things

--- Components/CourseList.jsx
+++ Components/CourseList.jsx
@@ -1,21 +1,19 @@
 import { getFilteredCourses, defaultFilter } from './Filter.js';
-import { View } from 'react-native';
+import { FlatList,ScrollView, View, Text } from 'react-native';
 import CourseCard from './Course.jsx';
 const CourseList = props => {
     let filter = defaultFilter;
     const filteredCourses = getFilteredCourses(filter);
     return (
-        <View>
-            <CourseCard code='PHIL 186'
-                course={filteredCourses['PHIL 186']} />
-        </View>
+        <ScrollView>
+            {/* <FlatList> */}
+                <Text>Hi</Text>
+                {Object.keys(filteredCourses).map((code) => (
+                    <CourseCard key={code} code={code} course={filteredCourses[code]} />
+                ))}
+            {/* </FlatList> */}
+        </ScrollView>
     );
 }
 
 export default CourseList;
-
-{/* <View>
-            {Object.keys(filteredCourses).map((code) => (
-                <CourseCard key={code} code={code} course={filteredCourses[code]} />
-            ))}
-        </View> */}
